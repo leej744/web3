912a3dc8c42cc2b51f36eabcdfd61e0f7f9655ad
julie <leej744@newschool.edu>
2013-09-11 19:08:30 -0400

    assignment

---
+++ assign1/js/main.js
@@ -0,0 +1,358 @@
+var imgArray = [  
+"images/001.jpg","images/002.jpg","images/003.jpg","images/004.jpg","images/005.jpg","images/006.jpg","images/007.jpg","images/008.jpg","images/009.jpg","images/010.jpg","images/011.jpg","images/012.jpg","images/013.jpg","images/014.jpg","images/015.jpg","images/016.jpg","images/017.jpg","images/018.jpg","images/019.jpg","images/020.jpg"
+];
+
+var imgArray2 = [  
+"images/111.png","images/111.jpg","images/112.jpg","images/113.jpg","images/114.jpg","images/115.jpg","images/116.jpg","images/117.jpg","images/118.jpg","images/119.jpg","images/120.jpg","images/121.jpg","images/122.jpg","images/123.jpg","images/124.jpg","images/125.jpg","images/126.jpg","images/127.jpg","images/128.jpg","images/129.jpg"];
+var counter1= 0; 
+	document.getElementById('mainImage').onclick = function () {  
+
+    document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+   	var images20 = document.getElementById("image20");
+   	images20.style.visibility = 'hidden';
+   		 
+	counter1 += 1;
+
+	document.getElementById('mainImage').onclick = function () { 
+    document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length];  
+    var images19 = document.getElementById("image19");
+    images19.style.visibility = 'hidden';
+	counter1+=1;
+
+		document.getElementById('mainImage').onclick = function () {  
+		document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+		var images18 = document.getElementById("image18");
+		images18.style.visibility = 'hidden';
+		counter1+=1;
+
+			document.getElementById('mainImage').onclick = function () {  
+				document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+				var images17 = document.getElementById("image17");
+				images17.style.visibility = 'hidden';
+				counter1+=1;
+						document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images16 = document.getElementById("image16");
+							images16.style.visibility = 'hidden';
+							counter1+=1;
+									document.getElementById('mainImage').onclick = function () {  
+										document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+										var images15 = document.getElementById("image15");
+										images15.style.visibility = 'hidden';
+										counter1+=1;
+										
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images14 = document.getElementById("image14");
+							images14.style.visibility = 'hidden';
+							counter1+=1;
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images13 = document.getElementById("image13");
+							images13.style.visibility = 'hidden';
+							counter1+=1;
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images12 = document.getElementById("image12");
+							images12.style.visibility = 'hidden';
+							counter1+=1;	
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images11 = document.getElementById("image11");
+							images11.style.visibility = 'hidden';
+							counter1+=1;		
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images10 = document.getElementById("image10");
+							images10.style.visibility = 'hidden';
+							counter1+=1;	
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images09 = document.getElementById("image09");
+							images09.style.visibility = 'hidden';
+							counter1+=1;
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images08 = document.getElementById("image08");
+							images08.style.visibility = 'hidden';
+							counter1+=1;
+
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images07 = document.getElementById("image07");
+							images07.style.visibility = 'hidden';
+							counter1+=1; 
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images06 = document.getElementById("image06");
+							images06.style.visibility = 'hidden';
+							counter1+=1; 
+
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images05 = document.getElementById("image05");
+							images05.style.visibility = 'hidden';
+							counter1+=1; 
+
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images04 = document.getElementById("image04");
+							images04.style.visibility = 'hidden';
+							counter1+=1; 
+
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images03 = document.getElementById("image03");
+							images03.style.visibility = 'hidden';
+							counter1+=1; 
+
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray[counter1 % imgArray.length]; 
+							var images02 = document.getElementById("image02");
+							images02.style.visibility = 'hidden';
+							counter1+=1; 
+									document.getElementById('mainImage').onclick = function () {  
+   	images20.style.visibility = 'visible';
+   	images19.style.visibility = 'visible';
+   	images18.style.visibility = 'visible';
+   	images17.style.visibility = 'visible';
+   	images16.style.visibility = 'visible';
+   	images15.style.visibility = 'visible';
+   	images14.style.visibility = 'visible';
+   	images13.style.visibility = 'visible';
+   	images12.style.visibility = 'visible';
+   	images11.style.visibility = 'visible';
+   	images10.style.visibility = 'visible';
+   	images09.style.visibility = 'visible';   	   	   	
+	images08.style.visibility = 'visible';
+   	images07.style.visibility = 'visible';
+   	images06.style.visibility = 'visible';
+   	images05.style.visibility = 'visible';
+   	images04.style.visibility = 'visible';
+   	images03.style.visibility = 'visible';
+   	images02.style.visibility = 'visible';
+
+
+
+   											var counter1= 0; 
+	document.getElementById('mainImage').onclick = function () {  
+
+    document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+   	var images20 = document.getElementById("image20");
+   	images20.style.visibility = 'hidden';
+   		 
+	counter1 += 1;
+
+	document.getElementById('mainImage').onclick = function () { 
+    document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length];  
+    var images19 = document.getElementById("image19");
+    images19.style.visibility = 'hidden';
+	counter1+=1;
+
+		document.getElementById('mainImage').onclick = function () {  
+		document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+		var images18 = document.getElementById("image18");
+		images18.style.visibility = 'hidden';
+		counter1+=1;
+
+			document.getElementById('mainImage').onclick = function () {  
+				document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+				var images17 = document.getElementById("image17");
+				images17.style.visibility = 'hidden';
+				counter1+=1;
+						document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images16 = document.getElementById("image16");
+							images16.style.visibility = 'hidden';
+							counter1+=1;
+									document.getElementById('mainImage').onclick = function () {  
+										document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+										var images15 = document.getElementById("image15");
+										images15.style.visibility = 'hidden';
+										counter1+=1;
+										
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images14 = document.getElementById("image14");
+							images14.style.visibility = 'hidden';
+							counter1+=1;
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images13 = document.getElementById("image13");
+							images13.style.visibility = 'hidden';
+							counter1+=1;
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images12 = document.getElementById("image12");
+							images12.style.visibility = 'hidden';
+							counter1+=1;	
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images11 = document.getElementById("image11");
+							images11.style.visibility = 'hidden';
+							counter1+=1;		
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images10 = document.getElementById("image10");
+							images10.style.visibility = 'hidden';
+							counter1+=1;	
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images09 = document.getElementById("image09");
+							images09.style.visibility = 'hidden';
+							counter1+=1;
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images08 = document.getElementById("image08");
+							images08.style.visibility = 'hidden';
+							counter1+=1;
+
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images07 = document.getElementById("image07");
+							images07.style.visibility = 'hidden';
+							counter1+=1; 
+							
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images06 = document.getElementById("image06");
+							images06.style.visibility = 'hidden';
+							counter1+=1; 
+
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images05 = document.getElementById("image05");
+							images05.style.visibility = 'hidden';
+							counter1+=1; 
+
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images04 = document.getElementById("image04");
+							images04.style.visibility = 'hidden';
+							counter1+=1; 
+
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images03 = document.getElementById("image03");
+							images03.style.visibility = 'hidden';
+							counter1+=1; 
+
+							document.getElementById('mainImage').onclick = function () {  
+							document.getElementById('mainImage').src = imgArray2[counter1 % imgArray2.length]; 
+							var images02 = document.getElementById("image02");
+							images02.style.visibility = 'hidden';
+							counter1+=1; 
+									document.getElementById('mainImage').onclick = function () {  
+									
+									window.location.reload(true);
+									
+									}
+
+}}}}}}}}}}}}}}}}}}}}
+}}}}}}}}}}}}}}}}}}}
+
+								 /*    
+    if (counter=4){
+    	var images17 = document.getElementById("image17");
+    	images17.style.visibility = 'hidden';
+	    }	  
+    if (counter=5){
+    	var images16 = document.getElementById("image16");
+    	images16.style.visibility = 'hidden';
+	    }	    
+    if (counter=6){
+    	var images15 = document.getElementById("image15");
+    	images15.style.visibility = 'hidden';
+	    }	      
+    if (counter=7){
+    	var images14 = document.getElementById("image14");
+    	images14.style.visibility = 'hidden';
+	    }	  
+    if (counter=8){
+    	var images13 = document.getElementById("image13");
+    	images13.style.visibility = 'hidden';
+	    }	  
+    if (counter=9){
+    	var images12 = document.getElementById("image12");
+    	images12.style.visibility = 'hidden';
+	    }
+    if (counter=10){
+    	var images11 = document.getElementById("image11");
+    	images11.style.visibility = 'hidden';
+	    }	    
+    if (counter=11){
+    	var images10 = document.getElementById("image10");
+    	images10.style.visibility = 'hidden';
+	    }	  
+    if (counter=12){
+    	var images09 = document.getElementById("image09");
+    	images09.style.visibility = 'hidden';
+	    }	    
+    if (counter=13){
+    	var images08 = document.getElementById("image08");
+    	images08.style.visibility = 'hidden';
+	    }	      
+    if (counter=14){
+    	var images07 = document.getElementById("image07");
+    	images07.style.visibility = 'hidden';
+	    }	  
+    if (counter=15){
+    	var images06 = document.getElementById("image06");
+    	images06.style.visibility = 'hidden';
+	    }	
+    if (counter=16){
+    	var images05 = document.getElementById("image05");
+    	images05.style.visibility = 'hidden';
+	    }	  
+    if (counter=17){
+    	var images04 = document.getElementById("image04");
+    	images04.style.visibility = 'hidden';
+	    }	    
+    if (counter=18){
+    	var images03 = document.getElementById("image03");
+    	images03.style.visibility = 'hidden';
+	    }	      
+    if (counter=19{
+    	var images02 = document.getElementById("image02");
+    	images02.style.visibility = 'hidden';
+	    }	  
+	if (counter=20){
+	    counter==0;
+	    images20.style.visibility = 'visible';
+	    images19.style.visibility = 'visible';
+	    images18.style.visibility = 'visible';
+	    images17.style.visibility = 'visible';
+	    images16.style.visibility = 'visible';
+	    images15.style.visibility = 'visible';	    		    
+	    images14.style.visibility = 'visible';
+	    images13.style.visibility = 'visible';
+	    images12.style.visibility = 'visible';
+	    images11.style.visibility = 'visible';
+	    images10.style.visibility = 'visible';	
+	    images09.style.visibility = 'visible';
+	    images08.style.visibility = 'visible';
+	    images07.style.visibility = 'visible';
+	    images06.style.visibility = 'visible';
+	    images05.style.visibility = 'visible';	    		    
+	    images04.style.visibility = 'visible';
+	    images03.style.visibility = 'visible';
+	    images02.style.visibility = 'visible';
+	    images01.style.visibility = 'visible';	    
+
+	    }
+}
+
+*/
